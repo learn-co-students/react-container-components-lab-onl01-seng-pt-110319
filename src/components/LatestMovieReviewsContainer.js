e86ebc39bf0bd67c95eb69ee9d939461a1448ebf
kchohee <kchohee0630@gmail.com>
2020-08-12 18:08:59 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
-import { fakeServerWithClock } from 'sinon';
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'

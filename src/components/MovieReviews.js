69284ec386ab6ac88a46ab6c9f1fc9b81af035db
kchohee <kchohee0630@gmail.com>
2020-08-12 18:08:08 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+    
+    const Review =({headline,byline,link,summary_short})=>{
+        return (
+            <div className="review">
+                <ul>
+                    <li>
+                        <a className="review-link" href={link.url}>{headline}</a>
+                        <p className="author">By: {byline}</p>
+                        <p> {summary_short}</p>
+                    </li>
+                </ul>
+            </div>
+        )
+    } 
+
+    return(
+        <div className='review-list'>
+                {reviews.map(Review)}
+        </div>
+    )
+    
+}
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,41 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component{
+    state={
+        reviews: [],
+        searchTerm: ''
+    }
+
+    render() {
+        return (
+            <div className="searchable-move-reviews">
+                <h1>Movie Reviews: </h1>
+                <form onSubmit= {this.submit}>    
+                    <input onChange= {this.onChange} type="text" placeholder="search"/> 
+                    <button type= "submit"> Search </button>
+                </form>
+                <MovieReviews reviews = {this.state.reviews} /> 
+            </div>
+        )
+    }
+
+    submit = (e) => {
+        e.preventDefault()
+        let search = URL + `&query=${this.state.searchTerm}`
+        fetch(search)
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+    onChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+}
+export default SearchableMovieReviewsContainer

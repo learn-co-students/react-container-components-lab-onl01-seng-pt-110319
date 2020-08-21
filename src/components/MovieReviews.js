b0e0b9ab437bd2dc13b72bbff09bd7a3c4fb30d0
github username <github email address>
2020-08-21 13:41:57 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({headline, link}) => {
+    return (
+        <div key={headline} className="review">
+            <a href={link.url} className="review-link">
+                {headline}
+            </a>
+        </div>
+    )
+}
+
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(Review)}</div>
+
+MovieReviews.defaultProps = { reviews: [] }
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,34 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    handleSearchTermChange = (e) => this.setState({ searchTerm: e.target.value })
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(URL.concat(this.state.searchTerm)).then(res => res.json()).then(data => this.setState({ reviews: data.results }))
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form className="search-form" onSubmit={this.handleSubmit}>
+                    <label htmlFor="search">Enter Search</label>
+                    <input id="search" type="text" onChange={this.handleSearchTermChange} />
+                    <button type="button">Search...</button>
+                </form>
+                < MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer;

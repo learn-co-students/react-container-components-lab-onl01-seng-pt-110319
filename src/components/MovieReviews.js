420c081f1d4fd1150291700c436d6696e84227e2
Alouiscious Media <info@reneesoul.com>
2020-09-08 20:59:30 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
+    <div className="review" key={headline}>
+      
+        <a className="review-link" href={link.url}>
+          {headline}
+        </a>
+        <p className="author" key={byline}>{byline}</p>
+        <span className="summary" key={summary_short}>{summary_short}</span>
+    </div>
+  )
+}
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>
+
+MovieReviews.defaultProps = {
+reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,37 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    searchTerm: '',
+    reviews: []
+  }
+
+  handleSearch = event =>
+  this.setState({ searchTerm: event.target.value })
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(URL.concat(this.state.searchTerm))
+    .then(resp => resp.json())
+    .then(data => this.setState({ reviews: data.results}))
+    .catch(error => {
+      console.error('Error for animal search - no result found:', error)
+    })
+  }
+
+  render() {
+    return(
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleSearch} placeholder="Search Movie Reviews" />
+          <button type="submit">Search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
